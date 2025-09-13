0ebe13d824eb7d1f696112d47e5e5bc8f062cad7
CaptainPax <101286587+Captainpax@users.noreply.github.com>
2025-09-13 05:23:39 -0700

    Add matrix rain overlay

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,11 +1,18 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import MatrixRain from './MatrixRain';
 import './style.css';
+
+// Force dark theme
+if (typeof document !== 'undefined') {
+  document.documentElement.classList.add('theme-dark');
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <MatrixRain />
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
+
