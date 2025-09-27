48ee8d87792036a3b86f129a3c73e4152f992fdf
CaptainPax <Tristan.J.Wickern@gamil.com>
2025-09-27 13:17:48 -0700

    Update import path in FusionChart constants and adjust meta tags in index.html

--- client/src/constants/fusionChart.js
+++ client/src/constants/fusionChart.js
@@ -4,7 +4,7 @@ import {
     FUSE_ARCANA_KEY_BY_LABEL,
     FUSE_ARCANA_ORDER,
     buildDefaultFusionChart,
-} from "../../../shared/fusionArcana.js";
+} from "@shared/fusionArcana.js";
 
 export { FUSION_ARCANA_METADATA, FUSE_ARCANA_LABEL_BY_KEY, FUSE_ARCANA_KEY_BY_LABEL, FUSE_ARCANA_ORDER };
 
