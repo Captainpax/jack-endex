266c7ac976add246ab46928e0a53e172a76dd490
CaptainPax <101286587+Captainpax@users.noreply.github.com>
2025-10-18 16:21:55 -0700

    Switch tests and linting to Vue tooling

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -1,1 +1,7 @@
-import "@testing-library/jest-dom/vitest";
+import '@testing-library/jest-dom/vitest';
+import { config } from '@vue/test-utils';
+
+config.global.stubs = {
+    transition: false,
+    'transition-group': false,
+};
